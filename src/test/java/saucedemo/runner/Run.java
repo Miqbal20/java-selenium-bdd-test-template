ee8c8f78853510f829b64cf37c73a4cba57dc82e
Muhammad Iqbal <miqbal020@gmail.com>
2023-10-21 03:35:39 +0700

    Initial Commit

--- src/test/java/saucedemo/runner/Run.java
+++ src/test/java/saucedemo/runner/Run.java
@@ -8,7 +8,7 @@ import org.junit.runner.RunWith;
 @CucumberOptions(
         features = "src/test/java/saucedemo/features",
         glue = "saucedemo.steps",
-        plugin = {"html:src/test/java/saucedemo/reports/HTML_report.html"}
+        plugin = {"html:src/test/java/saucedemo/report/HTML_report.html"}
 )
 
 public class Run {
